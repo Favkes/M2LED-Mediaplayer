1fc19abf983ea75f88070f92981bcf41a0bf49ea
Favkes <tho.strongest.avenger@gmail.com>
2025-07-18 20:22:22 +0200

    Add class-level tracking of global variable-bound slider.Slider objects to allow for compact Globals loading via the preset_loader.load_settings() function.

--- gui/slider.py
+++ gui/slider.py
@@ -1,12 +1,16 @@
 from utils.globals import Globals
 import tkinter as tk
 from utils.simple_logs import Logger, Logtype
+import numpy as np
 
 
 logger = Logger(__name__, 'blue')
 
 
 class Slider:
+    registered_sliders = []
+
+
     def __init__(self,
                  parent_frame: tk.Frame,
                  from_: int = 0,
@@ -14,19 +18,26 @@ class Slider:
                  start: int = 0,
                  orient: str = 'vertical',
                  showvalue: bool = False,
+                 showlabel: bool = True,
                  length: int = 100,
                  command = lambda x: None,
                  linked_global: str = None,
                  input_to_global_ratio: float = 1,
-                 label_text: str = 'Name') -> None:
+                 label_text: str = 'Name',
+                 value_label_format: str = '{}') -> None:
+
+        Slider.registered_sliders.append(self)
 
         self.parent = parent_frame
-        self.value = start
+        self.value = start / input_to_global_ratio
         self.from_ = from_
         self.to = to
         self.length = length
         self.linked_global = linked_global
         self.input_to_global_ratio = input_to_global_ratio
+        self.is_labeled = showlabel
+        self.is_vertical = orient == 'vertical'
+        self.value_format = value_label_format
 
         self.scale = tk.Scale(
             parent_frame,
@@ -34,10 +45,10 @@ class Slider:
             to=to,
             orient=orient,
             showvalue=showvalue,
-            length=100,
+            length=length,
             command=command,
         )
-        self.scale.set(start)
+        self.scale.set(self.value)
 
         self.label = tk.Label(
             self.parent,
@@ -46,22 +57,35 @@ class Slider:
 
         self.value_label = tk.Label(
             self.parent,
-            text=str(self.value)
+            text=self.value_format.format(self.value)
         )
+
 
     def grid(self,
              start_col: int = 0,
              start_row: int = 0,
              padx: int = 0) -> None:
-        self.label.grid(
-            column=start_col, row=start_row, padx=padx
-        )
+
+        widget_shift = np.array([1, 0])
+        if self.is_vertical:
+            widget_shift = np.array([0, 1])
+        widget_position = np.array([start_col, start_row])
+
+        if self.is_labeled:
+            self.label.grid(
+                column=widget_position[0], row=widget_position[1], padx=padx
+            )
+            widget_position += widget_shift
+
         self.scale.grid(
-            column=start_col, row=start_row + 1, padx=padx
+            column=widget_position[0], row=widget_position[1], padx=padx
         )
+        widget_position += widget_shift
+
         self.value_label.grid(
-            column=start_col, row=start_row + 2, padx=padx
+            column=widget_position[0], row=widget_position[1], padx=padx
         )
+        widget_position += widget_shift
 
 
     def update(self, val: int | float,
@@ -69,16 +93,16 @@ class Slider:
 
         if label_val:
             scale_val = round(val / self.input_to_global_ratio, 2)
-            label_val = round(val, 2)
+            self.value = round(val, 2)
         else:
             scale_val = round(val, 2)
-            label_val = round(val * self.input_to_global_ratio, 2)
+            self.value = round(val * self.input_to_global_ratio, 2)
 
         self.scale.set(scale_val)
-        self.value_label.config(text=str(label_val))
+        self.value_label.config(text=self.value_format.format(self.value))
 
         try:
-            label_val = type( getattr(Globals, self.linked_global) )(label_val)
-            setattr(Globals, self.linked_global, label_val)
+            self.value = type( getattr(Globals, self.linked_global) )(self.value)
+            setattr(Globals, self.linked_global, self.value)
         except Exception as e:
             logger.log(f'{e}', Logtype.error)
